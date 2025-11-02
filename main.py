31c4caf51d72e052af4e14da3fd1736961d8b75e
crescentchronicle <aalefort@outlook.com>
2025-11-02 14:12:07 -0800

    set up program window open and ability to close. set up .gitignore

--- main.py
+++ main.py
@@ -1,10 +1,32 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
+
+from constants import SCREEN_WIDTH
+from constants import SCREEN_HEIGHT
+from constants import ASTEROID_MIN_RADIUS
+from constants import ASTEROID_KINDS
+from constants import ASTEROID_SPAWN_RATE
+from constants import ASTEROID_MAX_RADIUS
+
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
 
 def main():
     print("Starting Asteroids!")
+    print("Screen width:", SCREEN_WIDTH)
+    print("Screen height:", SCREEN_HEIGHT)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        pygame.Surface.fill(screen, (0, 0, 0))
+        pygame.display.flip()
+
+
+
+
 
 
 if __name__ == "__main__":
