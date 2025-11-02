5e9d0f219b15cbc6876d27ba5e72fd5a7d9aba72
crescentchronicle <aalefort@outlook.com>
2025-11-02 15:12:57 -0800

    added circleshape class and player subclass, added rendered player in window

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ from constants import ASTEROID_MIN_RADIUS
 from constants import ASTEROID_KINDS
 from constants import ASTEROID_SPAWN_RATE
 from constants import ASTEROID_MAX_RADIUS
+from player import Player
 
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+dt = 0
 
 
 def main():
@@ -16,13 +19,22 @@ def main():
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
 
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
+
+
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 
 
