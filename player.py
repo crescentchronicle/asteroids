9de4658c03a6670c6421d2c77302cd97c3a55de0
crescentchronicle <aalefort@outlook.com>
2025-11-06 16:47:37 -0800

    switched self.radius and foward around in player.py to help interpreter not flag as error; also added newly required logger.py for project review

--- player.py
+++ player.py
@@ -12,9 +12,9 @@ class Player(CircleShape):
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
-        a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
+        a = self.position + self.radius * forward
+        b = self.position - self.radius * forward - right
+        c = self.position - self.radius * forward + right
         return [a, b, c]
 
 
